7ea0985a328dfdf7ee9bfbaeefc96817d1a07e3c
Ladislav Baco <laciKE3.14@gmail.com>
2014-03-25 18:14:01 +0100

    Move secret key to separate file

---
+++ wsgi/openshift/local_settings.py
@@ -0,0 +1,16 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+
+import os
+
+ON_OPENSHIFT = False
+if os.environ.has_key('OPENSHIFT_REPO_DIR'):
+	ON_OPENSHIFT = True
+
+secret_key_dir = os.path.dirname(os.path.realpath(__file__))
+if ON_OPENSHIFT:
+	secret_key_dir = os.environ['OPENSHIFT_DATA_DIR']
+
+with open(os.path.join(secret_key_dir, 'secret_key.txt')) as f:
+	MY_SECRET_KEY = f.read().strip()
+
+default_keys = { 'SECRET_KEY': MY_SECRET_KEY }
