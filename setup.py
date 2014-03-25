9b9b42d2d52b12b0c208229aea353e66f1e2abd0
Ladislav Baco <laciKE3.14@gmail.com>
2014-03-25 16:58:18 +0100

    Add MySQL-5.5 support and initial settings

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ if 'REDISCLOUD_URL' in os.environ and 'REDISCLOUD_PORT' in os.environ and 'REDIS
      packages.append('django-redis-cache')
      packages.append('hiredis')
 
-setup(name='YourAppName',
+setup(name='The Tourist Diary',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='OpenShift App for Tourists',
+      author='LaciKE',
+      author_email='lacike3.14+turistika@gmail.com',
       url='https://pypi.python.org/pypi',
       install_requires=packages,
 )
