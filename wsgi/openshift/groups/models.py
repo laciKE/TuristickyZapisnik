eec44fdf22248bdbb730432ed23cc2cb99cbc23b
Ladislav Baco <laciKE3.14@gmail.com>
2014-04-06 16:22:50 +0200

    Add view and validation for create group

--- wsgi/openshift/groups/models.py
+++ wsgi/openshift/groups/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
 
@@ -8,6 +9,15 @@ class CustomGroup(models.Model):
 	users = models.ManyToManyField(User, verbose_name=_('users'),
         blank=True, help_text=_('The users belongs to this group.'),
         related_name="custom_groups", related_query_name="group")
+
+	def validate_unique_name_owner(self):        
+		qs = CustomGroup.objects.filter(name=self.name)
+		if qs.filter(owner=self.owner).exists():
+			raise ValidationError(_('Name must be unique per user'))
+
+	def save(self):
+		self.validate_unique_name_owner()
+		super(CustomGroup, self).save()
 
 	def __unicode__(self):
 		return self.name + '__' + self.owner.username
--- wsgi/openshift/groups/views.py
+++ wsgi/openshift/groups/views.py
@@ -1,4 +1,5 @@
 from django.core.urlresolvers import reverse
+from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 from django.shortcuts import get_object_or_404, render, render_to_response
 from django.contrib.auth.decorators import login_required
@@ -7,6 +8,7 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect, HttpResponse
 from django.template.context import RequestContext
 from groups.models import CustomGroup
+from groups.forms import CustomGroupForm
 
 # Create your views here.
 
@@ -16,8 +18,26 @@ def index(request):
 	context = RequestContext(request)
 	user = request.user
 	groups = user.customgroup_set.all()
-	return render_to_response('groups/index.html', {'groups': groups}, context)
+	group_form = CustomGroupForm()
+
+	return render_to_response('groups/index.html', {'groups': groups, 'form': group_form}, context)
 
 @login_required
 def create(request):
+	if request.method == 'POST':
+		group_form = CustomGroupForm(data=request.POST)
+		if True and group_form.is_valid():
+			group = group_form.save(commit=False)
+			user = request.user
+			group.owner = user
+			try:
+				group.save()
+				messages.success(request, _('Successfully created group ') + group.name)
+			except ValidationError, e:
+				messages.error(request, e.message)
+		else:
+			messages.error(request, group_form.errors)
+	else:
+		group_form = CustomGroupForm()
+
 	return HttpResponseRedirect(reverse('groups:index'))
