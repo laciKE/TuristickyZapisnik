baeb3f27786ea6a6ab60bfeadaabfb8c937b09a3
Ladislav Baco <laciKE3.14@gmail.com>
2014-04-18 09:42:30 +0200

    Add model for trip

---
+++ wsgi/openshift/trips/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+
+from groups import views
+
+urlpatterns = patterns('',
+	#url(r'^$', views.index, name='index'),
+	#url(r'^create/$', views.create, name='create'),
+	#url(r'^delete/(?P<tripid>\d+)$', views.delete, name='delete'),
+	#url(r'^edit/(?P<tripid>\d+)/$', views.edit, name='edit'),
+)
