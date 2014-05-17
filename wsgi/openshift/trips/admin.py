9eaf75ad4a2fec8ba0312af82beea793d1052998
Ladislav Baco <laciKE3.14@gmail.com>
2014-05-17 15:19:43 +0200

    Change admin view: show Photo's thumbnails

--- wsgi/openshift/trips/admin.py
+++ wsgi/openshift/trips/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from trips.models import Trip, Photo, Comment
-# Register your models here.
+
+class PhotoAdmin(admin.ModelAdmin):
+	list_display = ('__unicode__', 'trip', 'title', 'thumbnail')
+
+	def thumbnail(self, photo):
+		return '<a href="' + photo.image.url + '"><img src="' + photo.thumb.url+'" alt="' + photo.title + '" width="40px" /></a>'
+	thumbnail.allow_tags = True
 
 admin.site.register(Trip)
-admin.site.register(Photo)
+admin.site.register(Photo, PhotoAdmin)
 admin.site.register(Comment)
