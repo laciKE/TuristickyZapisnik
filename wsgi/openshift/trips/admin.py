d2e6e1dd04b81ec360bc6d48af16f9f3a18649f1
Ladislav Baco <laciKE3.14@gmail.com>
2014-05-18 12:02:59 +0200

    Change trips photo admin view to include trip filter and title search

--- wsgi/openshift/trips/admin.py
+++ wsgi/openshift/trips/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from trips.models import Trip, Photo, Comment
 
 class PhotoAdmin(admin.ModelAdmin):
-	list_display = ('__unicode__', 'trip', 'title', 'thumbnail')
+	list_display = ['__unicode__', 'owner', 'trip', 'title', 'thumbnail']
+	list_filter = ['trip']
+	search_fields = ['title']
+
+	def owner(self, photo):
+		return photo.trip.owner
 
 	def thumbnail(self, photo):
 		return '<a href="' + photo.image.url + '"><img src="' + photo.thumb.url+'" alt="' + photo.title + '" width="40px" /></a>'
